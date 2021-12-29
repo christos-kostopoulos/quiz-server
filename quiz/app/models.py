cdb171ada1946c7196c09ad0c32ea8fdf60ed58a
kostop19 <kostop19@gmail.com>
2021-12-29 23:49:38 +0200

    first commit

---
+++ quiz/app/models.py
@@ -0,0 +1,51 @@
+import datetime
+from typing import Text
+
+from flask_appbuilder import Model
+from flask_appbuilder.models.decorators import renders
+from sqlalchemy import Column, Date, ForeignKey, Integer, String, Column, Text, Boolean
+from sqlalchemy.orm import relation, relationship
+from flask import Markup
+
+
+mindate = datetime.date(datetime.MINYEAR, 1, 1)
+
+class Quiz(Model): 
+  id = Column(Integer, primary_key=True)
+  name = Column(String(50), nullable=False)
+  category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+  category = relationship("Category")
+
+  def __repr__(self):
+        return self.name
+
+class Category(Model): 
+  id = Column(Integer, primary_key=True)
+  name = Column(String(50), unique=True, nullable=False)
+  
+  def __repr__(self):
+      return self.name
+
+class Question(Model): 
+  id = Column(Integer, primary_key=True)
+  questionTitle = Column(Text)
+  quiz_id = Column(Integer, ForeignKey("quiz.id"), nullable=False)
+  quiz = relationship("Quiz")
+  explanation = Column(Text)
+  # @renders('questionTitle')
+  # def latex_field(self):
+  #   # will render this columns as bold on ListWidget
+  #   return Markup('<b>' + self.questionTitle + '</b>')
+  
+  def __repr__(self):
+    return self.questionTitle
+  
+class Choice(Model): 
+  id = Column(Integer, primary_key=True)
+  choice = Column(Text)
+  is_right_choice =  Column(Boolean, default=False) 
+  question_id = Column(Integer, ForeignKey("question.id"), nullable=False)
+  question = relationship("Question")
+
+  def __repr__(self):
+    return self.choice
---
+++ quiz/app/views.py
@@ -0,0 +1,73 @@
+import calendar
+
+from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from . import appbuilder, db
+from .models import Category, Choice, Quiz, Question
+from flask_appbuilder.widgets import (
+    ListBlock, ListItem, ListLinkWidget, ListThumbnail, ShowBlockWidget
+)
+
+
+def fill_category():
+    try:
+        db.session.add(Category(name="Διακριτά μαθηματικά"))
+        db.session.add(Category(name="Τεχνολογία λογισμικου"))
+        Question.__table__.drop()
+        Quiz.__table__.drop()
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+
+def pretty_month_year(value):
+    return calendar.month_name[value.month] + " " + str(value.year)
+
+
+def pretty_year(value):
+    return str(value.year)
+
+class CategoryModelView(ModelView): 
+    datamodel = SQLAInterface(Category)
+    list_columns = ['id', 'name']
+
+class ChoiceModelView(ModelView): 
+    datamodel = SQLAInterface(Choice)
+    list_columns = ["id", "choice", "is_right_choice"]
+
+class QuestionModelView(ModelView): 
+    datamodel = SQLAInterface(Question)
+    list_columns = ["id", "latex_field"]
+    related_views = [ChoiceModelView]
+    
+    show_template = 'appbuilder/general/model/show_cascade.html'
+    edit_template = 'appbuilder/general/model/edit_cascade.html'
+
+class QuizModelView(ModelView):
+    datamodel = SQLAInterface(Quiz)
+    list_columns = ["category.name", "name" ]
+    related_views = [QuestionModelView]
+    list_widget = ListBlock
+    
+    
+    
+
+db.create_all()
+fill_category()
+
+appbuilder.add_view(
+    QuizModelView,
+    "Quiz",
+    icon="fa-folder-open-o",
+    category="Quiz",
+    category_icon="fa-envelope",
+)
+
+appbuilder.add_view_no_menu(
+    QuestionModelView
+)
+
+appbuilder.add_view_no_menu(
+    ChoiceModelView
+)
+
+appbuilder.add_view(CategoryModelView, "Categories", category="Quiz")
