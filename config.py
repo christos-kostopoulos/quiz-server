77e09975a51e6fc4829e69d196fa03926b5347ab
kostop19 <kostop19@gmail.com>
2022-01-01 22:35:05 +0200

    add database config url \

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = "\2\1thisismyscretkey\1\2\e\y\y\h"
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+# SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
 # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
